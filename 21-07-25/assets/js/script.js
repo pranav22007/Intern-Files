ad641bd4595a7f3d2d5f9c127b943172e26bde7b
pranav patil <pranavv.patil0712@gmail.com>
2025-07-24 22:41:32 +0530

    initial commit

---
+++ 21-07-25/assets/js/script.js
@@ -0,0 +1,25 @@
+function add(a,b){
+    return a + b;
+}
+console.log(add(5,10));
+
+
+const person ={
+    name: 'pranav',
+    age:25,
+}
+
+console.log( 'my name is ' + (person.name) + ' and my age is ' + (person.age) );
+
+
+let x
+in
+
+if (x>=18) {
+    console.log('your are allowed');
+    
+}
+else{
+    console.log('your are not allowed');
+    
+}
